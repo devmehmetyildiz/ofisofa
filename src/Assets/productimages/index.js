bae6658737ea5b69a924d2488b4842a532a71bf8
Mehmet yıldız <yildiz655@gmail.com>
2023-01-01 17:15:20 +0300

    Template Ended

---
+++ src/Assets/productimages/index.js
@@ -0,0 +1,43 @@
+import artemismasatakim1 from "./artemis masa takımı_01.jpg"
+import artemismasatakim2 from "./artemis masa takımı_02.jpg"
+import classmasatakim1 from "./class masa takımı_01.jpg"
+import classmasatakim2 from "./class masa takımı_02.jpg"
+import classmasatakim3 from "./class masa takımı_03.jpg"
+import informmasatakimshne1 from "./inform masa takımı shne_01.jpg"
+import informmasatakimshne2 from "./inform masa takımı shne_02.jpg"
+import inovamasatakim1 from "./inova masa takımı_01.jpg"
+import inovamasatakim2 from "./inova masa takımı_02.jpg"
+import safarimasatakim1 from "./safari masa takımı_01.jpg"
+import safarimasatakim2 from "./safari masa takımı_02.jpg"
+import smartmasatakim1 from "./smart masa takımı_01.jpg"
+import smartmasatakim2 from "./smart masa takımı_02.jpg"
+import trendmasatakim1 from "./trend masa takımı_01.jpg"
+import trendmasatakim2 from "./trend masa takımı_02.jpg"
+import yenimasatakim1_1 from "./yeni masa takımı 1_01.jpg"
+import yenimasatakim1_2 from "./yeni masa takımı 1_02.jpg"
+import yenimasatakim2_1 from "./yeni masa takımı 2_01.jpg"
+import yenimasatakim2_2 from "./yeni masa takımı 2_02.jpg"
+import yenimasatakim2_3 from "./yeni masa takımı 2_03.jpg"
+
+export default {
+    artemismasatakim1,
+    artemismasatakim2,
+    classmasatakim1,
+    classmasatakim2,
+    classmasatakim3,
+    informmasatakimshne1,
+    informmasatakimshne2,
+    inovamasatakim1,
+    inovamasatakim2,
+    safarimasatakim1,
+    safarimasatakim2,
+    smartmasatakim1,
+    smartmasatakim2,
+    trendmasatakim1,
+    trendmasatakim2,
+    yenimasatakim1_1,
+    yenimasatakim1_2,
+    yenimasatakim2_1,
+    yenimasatakim2_2,
+    yenimasatakim2_3,
+}
---
+++ src/Assets/products/index.js
@@ -0,0 +1,70 @@
+import productimages from "../productimages";
+
+export const datas = [
+    {
+        category: "Masa Takımı",
+        product: 'Artemis masa takımı',
+        imgs: [
+            productimages.artemismasatakim1,
+            productimages.artemismasatakim2,
+        ]
+    },
+    {
+        category: "Masa Takımı",
+        product: 'Class Masa Takımı',
+        imgs: [
+            productimages.classmasatakim1,
+            productimages.classmasatakim2,
+            productimages.classmasatakim3,
+        ]
+    },
+    {
+        category: "Masa Takımı",
+        product: 'İnform Masa Takımı',
+        imgs: [
+            productimages.informmasatakimshne1,
+            productimages.informmasatakimshne2,
+        ]
+    },
+    {
+        category: "Masa Takımı",
+        product: 'Safari Masa Takımı',
+        imgs: [
+            productimages.safarimasatakim1,
+            productimages.safarimasatakim2,
+        ]
+    },
+    {
+        category: "Masa Takımı",
+        product: 'Smart Masa Takımı',
+        imgs: [
+            productimages.smartmasatakim1,
+            productimages.smartmasatakim2,
+        ]
+    },
+    {
+        category: "Masa Takımı",
+        product: 'Trend Masa Takımı',
+        imgs: [
+            productimages.trendmasatakim1,
+            productimages.trendmasatakim2,
+        ]
+    },
+    {
+        category: "Masa Takımı",
+        product: 'Yeni Masa Takımı 1',
+        imgs: [
+            productimages.yenimasatakim1_1,
+            productimages.yenimasatakim1_2,
+        ]
+    },
+    {
+        category: "Masa Takımı",
+        product: 'Yeni Masa Takımı 2',
+        imgs: [
+            productimages.yenimasatakim2_1,
+            productimages.yenimasatakim2_2,
+            productimages.yenimasatakim2_3,
+        ]
+    }
+]
